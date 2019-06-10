07b8fe91289a8f89c92e800f02138c368be6b29a
NeverEndingCycle <troypradford@gmail.com>
2019-06-09 22:58:53 -0700

    Initial commit. Sorts word_list into easy, norm, and hard files.

---
+++ hangman_data/tools/word_sorter.py
@@ -0,0 +1,37 @@
+easy=[]
+norm=[]
+hard=[]
+
+with open('word_list', 'r') as word_list:
+    word_list = word_list.readlines()
+
+for word in word_list:
+    newword = word[:-1]
+    length = len(newword)
+
+    if length in range(5, 7):
+        easy.append(word)
+        print(f'{word} appended to easy_list.')
+    elif length in range(7, 9):
+        norm.append(word)
+        print(f'{word} appended to norm_list.')
+    elif length >= 9:
+        hard.append(word)
+        print(f'{word} appended to hard_list.')
+    else:
+        print(f"ERROR: '{word}' failed to append to a list.")
+
+with open('easy', 'w') as easy_file:
+    for word in easy:
+        easy_file.write(word)
+        print(f'{word} appended to easy_file.')
+
+with open('norm', 'w') as norm_file:
+    for word in norm:
+        norm_file.write(word)
+        print(f'{word} appended to norm_file.')
+
+with open('hard', 'w') as hard_file:
+    for word in hard:
+        hard_file.write(word)
+        print(f'{word} appended to hard_file.')
