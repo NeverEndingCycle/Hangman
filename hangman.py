5244e759996a5156b9cd7769b2c469f4d90cc26f
NeverEndingCycle <troypradford@gmail.com>
2019-06-13 14:40:17 -0700

    Finished v1 of hangman.

--- hangman.py
+++ hangman.py
@@ -3,7 +3,7 @@ from time import sleep as slp
 from os import system as sys
 
 class startup:
-    def show_welcome_screen():
+    def welcome():
         splash = [
                 '+===============+',
                 '|               |',
@@ -18,63 +18,39 @@ class startup:
             slp(.5)
         slp(1.5)
         sys('cls')
-        print('Welcome to hangman. By: N.E.C and Decoy.', end='\n\n>>> Press enter to continue.')
+        print('Welcome to hangman. By: NeverEndingCycle.', end='\n\n>>> Press enter to continue.')
         pause = input()
         sys('cls')
 
 class function:
-    def get_dif():
-        print('Please choose your difficulty.\n\n', end='')
-        slp(.5)
-        print('Easy:   (5-6 letter words)\n', end='')
-        slp(.5)
-        print('Normal: (7-8 letter words)\n', end='')
-        slp(.5)
-        print('Hard:   (9+ letter words)\n', end='')
-        slp(.5)
-        print('')
-        slp(.5)
-        print('\n[Easy, Normal, Hard]', end='\n>>> ')
-        while True:
-            diff = input()
-            diff = diff.lower()
-            if diff != 'easy' and diff != 'normal' and diff != 'hard':
-                print('Invalid choice.')
-                print('\n>>> ', end='')
-            else:
-                break
-        sys('cls')
-        return diff
-
     def fetch_word(difficulty):
         if difficulty == 'easy':
-            while True:
-                with open('hangman_data/easy', 'r') as list_file:
-                    word_list = list_file.readlines()
-                word = choice(word_list)
-                word = ''.join(word.split())
-                return word
+            with open('hangman_data/easy', 'r') as list_file:
+                word_list = list_file.readlines()
+            word = choice(word_list)
+            word = ''.join(word.split())
+            return word
         elif difficulty == 'normal':
-            while True:
-                with open('hangman_data/norm', 'r') as list_file:
-                    word_list = list_file.readlines()
-                word = choice(word_list)
-                word = ''.join(word.split())
-                return word
+            with open('hangman_data/norm', 'r') as list_file:
+                word_list = list_file.readlines()
+            word = choice(word_list)
+            word = ''.join(word.split())
+            return word
         elif difficulty == 'hard':
-            while True:
-                with open('hangman_data/hard', 'r') as list_file:
-                    word_list = list_file.readlines()
-                word = choice(word_list)
-                word = ''.join(word.split())
-                return word
+            with open('hangman_data/hard', 'r') as list_file:
+                word_list = list_file.readlines()
+            word = choice(word_list)
+            word = ''.join(word.split())
+            return word
 
     def get_globals(word):
-        global char_list, word_print
+        global char_list, word_print, guessed
         char_list = list(word)
 
         length = function.get_length(word)
         word_print = list(length)
+
+        guessed = []
 
     def picture(bad_guesses):
         bad_guesses = str(bad_guesses)
@@ -96,9 +72,22 @@ class function:
         bad_guesses += 1
         return bad_guesses
 
+    def validate_guess(guess):
+        invalid = False
+        if guess in guessed:
+            invalid = 'already guessed'
+
+        elif guess == '' or not guess.isalpha():
+            invalid = True
+
+        else:
+            guessed.append(guess)
+        return invalid
+
     def check_guess(word, guess, bad_guesses):
         letter_length = function.get_length(word)
         char_index = None
+        victory = None
 
         if guess in char_list:
             char_index = [i for i in range(len(char_list)) if char_list[i] == guess]
@@ -108,37 +97,107 @@ class function:
                 word_print[i] = guess
 
         elif guess == word:
-            print('You win!')
-        else:
-            function.add_guess(bad_guesses)
+            victory = True
 
-        return bad_guesses, word_print
+        else:
+            bad_guesses = function.add_guess(bad_guesses)
+
+        return bad_guesses, word_print, victory
 
 class game:
-    def start(word):
+    def get_dif():
+        print('Please choose your difficulty.\n\n', end='')
+        slp(.5)
+        print('Easy:   (5-6 letter words)\n', end='')
+        slp(.5)
+        print('Normal: (7-8 letter words)\n', end='')
+        slp(.5)
+        print('Hard:   (9+ letter words)\n', end='')
+        slp(.5)
+        print('')
+        slp(.5)
+        print('\n[Easy, Normal, Hard]', end='\n>>> ')
+        while True:
+            dif = input()
+            dif = dif.lower()
+            if dif != 'easy' and dif != 'normal' and dif != 'hard':
+                print('Invalid choice.', end='')
+                print('\n>>> ', end='')
+            else:
+                break
+        sys('cls')
+        return dif
+
+    def start(dif):
+        word = function.fetch_word(dif)
         function.get_globals(word)
+        victory = None
         bad_guesses = 0
         guess = None
         char_index = None
         word_print = None
+        new_word_print = None
+        invalid = True
 
         while True:
             for pic in function.picture(bad_guesses):
                 print(pic, end='')
 
-            print(word)
+            if bad_guesses == 7:
+                victory = False
+                return word, victory
+
+            elif victory == True:
+                return word, victory
 
             if word_print == None:
                 new_word_print = function.get_length(word)
-                print(f'{new_word_print}')
             else:
                 new_word_print = ''
                 for i in word_print:
                     new_word_print += i
+
+            if new_word_print == word:
+                victory = True
+                return word, victory
+            else:
                 print(f'{new_word_print}')
 
-            guess = input('\n\nGuess a letter or word.\n>>> ')
-            guess = guess.lower()
+            while True:
+                guess = input('\n\nGuess a letter or word.\n>>> ')
+                guess = guess.lower()
 
-            bad_guesses, word_print = function.check_guess(word, guess, bad_guesses)
+                invalid = function.validate_guess(guess)
+
+                if invalid == True:
+                    print('Invalid Answer.', end='')
+                elif invalid == 'already guessed':
+                    print("You can't guess the same answer twice.", end='')
+                else:
+                    bad_guesses, word_print, victory = function.check_guess(word, guess, bad_guesses)
+                    break
+
             sys('cls')
+
+    def cont(word, victory):
+        if victory == True:
+            print(f"\nCongradulations, you won!\n\nThe word was: '{word}'\n")
+        elif victory == False:
+            print(f"\nBummer, you lost!\n\nThe word was: '{word}'\n")
+        while True:
+            print("Would you like to play another match?")
+            answer = input('\n[Yes, No]\n>>> ')
+            answer.lower()
+
+            if answer == 'yes' or  answer == 'no':
+                sys('cls')
+                break
+            else:
+                print('Invalid Choice.\n')
+
+        if answer == 'yes':
+            cont = True
+        elif answer == 'no':
+            cont = False
+
+        return cont
